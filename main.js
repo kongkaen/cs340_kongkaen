a4be862f8dcfe83c36acd6d2db55f34ed6b5c7f0
kongkaen <56180077+kongkaen@users.noreply.github.com>
2020-05-27 01:35:08 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ app.engine('handlebars', handlebars.engine);
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/static', express.static('public'));
 app.set('view engine', 'handlebars');
-app.set('port', process.env.port || 4000);
+app.set('port', process.argv[2]);
 app.set('mysql', mysql);
 app.use('/employee', require('./employee.js'));
 app.use('/customer', require('./customer.js'));
